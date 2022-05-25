e37032e39a177bd977108bfc6bedf0001277bb00
Gyorilaszlo18 <gyorilaszlo18@gmail.com>
2022-05-25 12:37:57 +0200

    Add application skeleton

---
+++ src/test/java/hu/nye/progkornyezet/vasarlasilista/VasarlasilistaApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nye.progkornyezet.vasarlasilista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VasarlasilistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
